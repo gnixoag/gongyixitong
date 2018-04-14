f850ae539f39faae37be073506cc8fd39d257f6c
gnixoag <gnix.oag@gmail.com>
2018-04-14 09:05:31 +0800

    在程序中设定本模块的位置

--- gongyixitong/main.py
+++ gongyixitong/main.py
@@ -1,3 +1,8 @@
+#把本模块路径加入目录
+import sys,os
+sys.path.append(os.path.dirname(sys.path[0]))
+
+#正常代码
 from PyQt5.QtWidgets import *
 
 from gongyixitong.widgets.login import *
@@ -5,7 +10,7 @@ from gongyixitong.widgets.mainUI import *
 from gongyixitong.widgets.usermanageUI import UserManageUI
 
 if __name__ == '__main__':
-    import sys
+
     app = QApplication(sys.argv)
     login = UI_login()
     if login.exec_():
